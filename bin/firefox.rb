d345544c739b91129df59b4a1a0f0b99a4ec1655
Ji-Yuhang <jiyuhangzhengzhou@qq.com>
2019-02-18 15:58:23 +0800

    watir open url when sinatra receive word

---
+++ bin/firefox.rb
@@ -0,0 +1,32 @@
+require 'sinatra'
+require 'sinatra/base'
+require 'watir'
+require 'watir-webdriver'
+require 'colorize'
+require 'nokogiri'
+require 'uri'
+
+# Login Process ---
+puts ("Firefox Started...").yellow
+$browser = Watir::Browser.new :firefox
+class App < Sinatra::Base
+set :bind, '0.0.0.0'
+set :environment, :production
+
+get '/' do
+  word =  params['word']
+  url = "https://cn.bing.com/images/search?q=#{word}"
+  uri = URI.encode(url)
+  $browser.goto(uri)
+  word
+end
+get '/anki' do
+  word =  params['word']
+  url = "https://memorysheep.com/baicizhans/anki?word=#{word}"
+  uri = URI.encode(url)
+  $browser.goto(uri)
+  word
+end
+end
+
+App.run!
